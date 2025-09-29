49264ee2e8dee7e284c41cd80b12a36200d84d03
juluasutosh-design <juluasutosh@gmail.com>
2025-09-29 16:48:56 +0530

    Update main.tf

--- 03-Terra/main.tf
+++ 03-Terra/main.tf
@@ -1,1 +1,7 @@
-
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "baghazurerm_resource_group" {
+  name = "baghazurerm_resource_group-${}"
+resource "azurerm_virtual_network" "baghazurerm_virtual_network" {
+  
