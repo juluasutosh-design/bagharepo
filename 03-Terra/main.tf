7adcc3ae9b8bbe6db8215ed3033c8369783fdb80
juluasutosh-design <juluasutosh@gmail.com>
2025-09-29 19:05:41 +0530

    Update main.tf

--- 03-Terra/main.tf
+++ 03-Terra/main.tf
@@ -6,14 +6,14 @@ resource "random_id" "baghazurerm_suffix_gen" {
         byte_length = 6
     }
 
-resource "azurerm_resource_group" "baghazurerm_resource_group" {
+resource "azurerm_resource_group" "bagharg" {
   name = "baghazurerm_resource_group-${random_id.baghazurerm_suffix_gen.hex}"
   location = "India Central"
    }
 
-resource "azurerm_virtual_network" "baghazurerm_virtual_network" {
+resource "azurerm_virtual_network" "baghavn {
   name = "baghazurerm_virtual_network-${random_id.baghazurerm_suffix_gen.hex}"
-  resource_group = azurerm_resource_group.baghazurerm_resource_group.name
+  resource_group_name = azurerm_resource_group.baghazurerm_resource_group.name
   location = azurerm_resource_group.baghazurerm_resource_group.location
   address_space       = ["10.0.0.0/16"]
 }
