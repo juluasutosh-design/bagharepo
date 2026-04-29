25ee4daaef6ebf0381c7c4c0331f5dd18d75e5e3
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 00:35:19 +0530

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,6 +1,6 @@
 const YAML = require('yaml');
 const fs = require('fs');
-for ( const env of process.env.myArray ) {
+for ( const env of process.env.envArray ) {
   try {
   const file = YAML.parse(fs.readFileSync(`./deployment/aks/chart/process.env.${yaml}`));
   console.log(file.julu-cname.env);
