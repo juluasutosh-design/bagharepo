ddd2c8a5b970b1f3ec5ce067a0fea12e0038154f
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 00:34:25 +0530

    Update myfile.js

--- myfile.js
+++ myfile.js
@@ -1,1 +1,13 @@
-
+const YAML = require('yaml');
+const fs = require('fs');
+for ( const env of process.env.myArray ) {
+  try {
+  const file = YAML.parse(fs.readFileSync(`./deployment/aks/chart/process.env.${yaml}`));
+  console.log(file.julu-cname.env);
+  
+}
+  catch error(e) {
+    console.log("No such valid env Selected in Actions Input");
+  }
+};
+    
