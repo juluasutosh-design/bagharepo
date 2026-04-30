ecaf62d56e93334b2b92ca55db4ecbdf5a10c10a
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 23:41:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ console.log(process.argv)
 for ( const env of myArray ) {
   const file = fs.readFileSync(`deployment/aks/chart/${env}.values.yaml`,'utf8');
   const config = YAML.parse(file);
-  console.log(config['julu-cname.yamlVal']);
+  console.log(config['julu-cname'].yamlVal);
   /*
   if ( process.argv.slice(4,5) === 'undefined' && ( process.argv.slice(4,5) ||  process.argv.slice(5,6) )) {
     
