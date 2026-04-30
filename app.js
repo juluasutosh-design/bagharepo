2f622878622771d91f49a62851513d8ef45d800b
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 19:34:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 console.log("myfile is running....");
 const YAML = require('yaml');
 const fs = require('fs');
-const myArray = process.argv.slice(2)
+const dot = require('dot');
+const myArray = process.argv.slice(2,3)
 console.log(myArray)
+/*
 for ( const env of myArray ) {
   const file = fs.readFileSync(`deployment/aks/chart/${env}.values.yaml`,'utf8');
-  const config = YAML.parse(file)
-  console.log(config['julu-cname'].env);
+  const config = YAML.parse(file);
+  //console.log(config['julu-cname'].env);
+  if ( process.argv.slice(4,5) === 'undefined' && ( process.argv.slice(4,5) ||  process.argv.slice(5,6) )) {
+    
+  }
+  else if ( process.argv(3,4) && process.argv(4,5) ) {
+    dot.dot(process.argv.slice(3,4)) = process.argv.slice(4,5);
+  else {
+    console.log("if you want to overwtite , Please provide both values & input yaml")
+  }
 };
+*/
 console.log("passedimport step");
+
