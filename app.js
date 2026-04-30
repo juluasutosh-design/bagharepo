6c80dd2a4b14b1ba2cb9056d156b56e4b791ee1a
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 15:32:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 console.log("myfile is running....");
 const YAML = require('yaml');
-const FS = require('fs');
-console.log("passedimport step")
+const fs = require('fs');
+for ( const env of process.env.envArray ) {
+  try {
+  const file = YAML.parse(fs.readFileSync(`./deployment/aks/chart/${env}.values.yaml`));
+  console.log(file.julu-cname.env);
+  
+}
+  catch error(e) {
+    console.log("No such valid env Selected in Actions Input");
+  }
+};
+console.log("passedimport step");
