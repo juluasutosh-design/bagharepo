02c270b60d7988f3b97fcc261340f4558733484d
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 15:51:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 console.log("myfile is running....");
 const YAML = require('yaml');
 const fs = require('fs');
+/*
 for ( const env of process.env.envArray ) {
   const file = YAML.parse(fs.readFileSync(`./deployment/aks/chart/${env}.values.yaml`));
   console.log(file.julu-cname.env);
 };
+*/
 console.log("passedimport step");
