069365914d9b1d235ab99645ddc6ffd104f6c0d7
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 16:51:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ console.log(myArray)
 for ( const env of myArray ) {
   const file = fs.readFileSync(`deployment/aks/chart/${env}.values.yaml`,'utf8');
   const config = YAML.parse(file)
-  console.log(config.julu-cname.env);
+  console.log(config[julu-cname].env);
 };
 console.log("passedimport step");
