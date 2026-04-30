5e4edf2c23c5c27d25b4739c7adb6532890c0c01
juluasutosh-design <juluasutosh@gmail.com>
2026-04-30 23:50:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 console.log("myfile is running....");
 const YAML = require('yaml');
 const fs = require('fs');
-const myArray = process.argv[2].split(',')
-const yamlVal = process.argv[3]
-const absVal = process.argv[4]
+const myArray = process.argv[2].split(',');
+const yamlVal = process.argv[3];
+const absVal = process.argv[4];
 console.log(myArray);
 console.log(absVal);
 console.log(yamlVal);
-console.log(process.argv)
+console.log(process.argv);
 for ( const env of myArray ) {
   const file = fs.readFileSync(`deployment/aks/chart/${env}.values.yaml`,'utf8');
   const config = YAML.parse(file);
-  console.log(config['julu-cname'].yamlVal);
+  const base = config['julu-cname'];
+  console.log(base.yamlVal);
   console.log(config['julu-cname'].env);
   /*
   if ( process.argv.slice(4,5) === 'undefined' && ( process.argv.slice(4,5) ||  process.argv.slice(5,6) )) {
