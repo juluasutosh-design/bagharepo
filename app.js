9d2cb4e4ba7c776cdb5dcaf8f621938d8404dbdb
juluasutosh-design <juluasutosh@gmail.com>
2026-05-05 02:51:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-console.log("myfile is running....");
-const YAML = require('yaml');
-const fs = require('fs');
-const myArray = process.argv[2].split(',');
-const yamlVal = process.argv[3];
-const absVal = process.argv[4];
-console.log(myArray);
-console.log(absVal);
-console.log(yamlVal);
-console.log(process.argv);
-for ( const env of myArray ) {
-  const file = fs.readFileSync(`deployment/aks/chart/${env}.values.yaml`,'utf8');
-  const config = YAML.parse(file);
-  console.log(config['julu-cname'][yamlVal]);
-  console.log(config['julu-cname'].env);
-  /*
-  if ( process.argv.slice(4,5) === 'undefined' && ( process.argv.slice(4,5) ||  process.argv.slice(5,6) )) {
-    
-  }
-  else if ( process.argv(3,4) && process.argv(4,5) ) {
-    dot.dot(process.argv.slice(3,4)) = process.argv.slice(4,5);
-  else {
-    console.log("if you want to overwtite , Please provide both values & input yaml")
-  }
-*/  
-};
-console.log("passedimport step");
+const yaml = require('yaml');
+const fs   = require('fs');
 
+
+myObj = yaml.safeLoad(fs.readFileSync('./deployment/aks/chart/myfile.yml', 'utf8'));
+console.log(myObj);
+function setByPath(keyPath) {
+  const segments = keyPath
+    .split(".")
+    .map((part) => part.trim());
+    return segments
+}
+
+console.log(setByPath("a.b.c"))
